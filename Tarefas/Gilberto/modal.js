e629dc9da9a1c07ff6ac0b9feb265aab0168d244
Gilberto Sá <gibasa@gmail.com>
2023-01-20 08:58:22 -0300

    Bug arrumado. Modal funcionando. Falta estilizar

--- Tarefas/Gilberto/modal.js
+++ Tarefas/Gilberto/modal.js
@@ -2,12 +2,22 @@ var servicosModalButton = document.querySelector(".servicos-modal-btn");
 var servicosModalButtonFechar = document.querySelector(".close-modal-servicos");
 var servicosModal = document.querySelector(".servicos-modal");
 var servicosModalFade = document.querySelector(".fade-servicos-modal");
-var toggleModal = () => {
+var resumoModalButton = document.querySelector(".resumo-modal-btn");
+var resumoModal = document.querySelector(".resumo-modal");
+var resumoModalFade = document.querySelector(".fade-resumo-modal");
+var toggleModalServico = () => {
   servicosModal.classList.toggle("hide");
   servicosModalFade.classList.toggle("hide");
 };
+var toggleModalResumo = () => {
+  resumoModal.classList.toggle("hide-resumo");
+  resumoModalFade.classList.toggle("hide-resumo");
+};
 [servicosModalButton, servicosModalFade, servicosModalButtonFechar].forEach(
   (element) => {
-    element.addEventListener("click", () => toggleModal());
+    element.addEventListener("click", () => toggleModalServico());
   }
 );
+[resumoModalButton, resumoModalFade].forEach((element) => {
+  element.addEventListener("click", () => toggleModalResumo());
+});

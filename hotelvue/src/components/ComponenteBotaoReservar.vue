890b58473c8ca43fd55d03221300cf0ebe21569c
Hewerton <hewerton.fl@gmail.com>
2023-01-29 17:11:56 -0300

    Limpeza de estilos obsoletos em componente form contatos e botão reservar

--- hotelvue/src/components/ComponenteBotaoReservar.vue
+++ hotelvue/src/components/ComponenteBotaoReservar.vue
@@ -7,15 +7,3 @@ export default {
     name: 'ComponenteBotaoReservar',
 }
 </script>
-
-<style scoped>
-.btn-blue {
-    font-size: 1rem;
-    padding: 1rem 1rem;
-    border: none;
-    background-color: #176485;
-    color: #ffffff;
-    text-transform: uppercase;
-    cursor: pointer;
-}
-</style>

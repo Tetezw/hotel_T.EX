1d43fe53b7e965e4346906f545de2467d36eb4c1
Fagner Farias <60819320+ffagner@users.noreply.github.com>
2023-02-01 07:59:40 -0300

    criado a estrutura de cards para a pagina acomodações

--- hotelvue/src/App.vue
+++ hotelvue/src/App.vue
@@ -1,20 +1,34 @@
 <template>
+  <main>
+    <ComponenteHeader/>
+    <CardTemplate/>
+    <ComponeteFooter/>
+  </main>
+
   <!-- <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/> -->
-  <section>
+  </nav> -->
+  <!-- <router-view/> -->
+  <!-- <section>
     <h1>Hotel Recanto dos Códigos</h1>
-  </section>
+  </section> -->
 </template>
 
 <script>
+import CardTemplate from "./components/CardTemplate.vue"
+import ComponeteFooter from "./components/ComponenteFooter.vue"
+import ComponenteHeader from "./components/ComponenteHeader.vue";
+
 export default {
   name: "App",
   components: {
-  },
-};
+    CardTemplate,
+    ComponeteFooter,
+    ComponenteHeader
+
+  }
+}
 </script>
 
 <style lang='scss' src="./scss/style.scss" />

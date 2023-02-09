92266abd6bff8482fa791afc171833564d8257ae
terezazw <terezawisser@gmail.com>
2023-02-09 18:17:04 -0300

    projeto compilado

---
+++ hotelvue/deploy.sh
@@ -0,0 +1,20 @@
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Tetezw/hotel_T.EX.git main:gh-pages
+
+cd -
--- hotelvue/vue.config.js
+++ hotelvue/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
 })
+
+module.exports = {
+    publicPath:
+        process.env.NODE_ENV === 'production' ? '/hotel_T.EX/hotelvue' : '/',
+}

f3e6286de7a1fd17fe64249cee267cfaf2818cde
terezazw <terezawisser@gmail.com>
2023-02-09 20:56:23 -0300

    Projeto compilado

--- hotelvue/src/router/index.js
+++ hotelvue/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes = [
   {
@@ -10,38 +10,39 @@ const routes = [
   {
     path: "/sobre",
     name: "sobre",
-    component: () => import("../views/SobreView.vue"),
+    component: () => import("@/views/SobreView.vue"),
   },
   {
     path: "/acomodacoes",
     name: "acomodacoes",
-    component: () => import("../views/AcomodacoesView.vue"),
+    component: () => import("@/views/AcomodacoesView.vue"),
   },
   {
     path: "/contato",
     name: "contato",
-    component: () => import("../views/ContatoView.vue"),
+    component: () => import("@/views/ContatoView.vue"),
   },
   {
     path: "/login",
     name: "login",
-    component: () => import("../views/LoginView.vue"),
+    component: () => import("@/views/LoginView.vue"),
   },
   {
     path: "/minhasreservas",
     name: "minhasReservas",
-    component: () => import("../views/MinhasReservasView.vue"),
+    component: () => import("@/views/MinhasReservasView.vue"),
   },
   {
     path: "/reserva",
     name: "reserva",
-    component: () => import("../views/ReservaView.vue"),
+    component: () => import("@/views/ReservaView.vue"),
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+    // mode: 'hash',
+})
 
 export default router;
